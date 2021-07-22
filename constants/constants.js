0e1683a4b93e46eba4e946a3c190dba7477b2ac3
paul-swiecicki <paul.swiecicki2@gmail.com>
2021-07-22 21:45:14 +0200

    add resized directory removal action
    directories named by uids are now removed after time specified in constant

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,16 @@
 const IMAGE_RESIZE_SIZES = [100, 200, 400];
 const COMPATIBLE_FORMATS = [".png", ".jpg", ".jpeg"];
 
-module.exports = { IMAGE_RESIZE_SIZES, COMPATIBLE_FORMATS };
+const SEC = 1000;
+const MIN = SEC * 60;
+/**
+ * When image is uploaded, resized and placed in uid named directory,
+ * after this amount of time the directory will be removed.
+ */
+const RESIZED_DIR_REMOVAL_DELAY = MIN * 20;
+
+module.exports = {
+  IMAGE_RESIZE_SIZES,
+  COMPATIBLE_FORMATS,
+  RESIZED_DIR_REMOVAL_DELAY,
+};
--- helpers/resizeImage.js
+++ helpers/resizeImage.js
@@ -2,18 +2,18 @@ const sharp = require("sharp");
 const path = require("path");
 const fs = require("fs");
 const { ensureExists } = require("./ensureExists");
+const { RESIZED_DIR_REMOVAL_DELAY } = require("../constants/constants");
 
 // make sure that sharp doesn't use images stored in cache sent earlier by other users
 sharp.cache({ files: 0 });
 
 /**
- *
- *
  * @param {Object} obj
- * @param {String} obj.outputDirectory
- * @param {String} obj.imageSize
+ * @param {String} obj.outputDirectory path to main output directory
+ * @param {Number|String} obj.size
  * @param {String} obj.imageExtension
- * @returns Output path (including image istelf) for resized images
+ * @param {String} obj.uid
+ * @returns Output path (including image istelf) for resized image
  */
 const getResizedImagePath = ({
   outputDirectory,
@@ -40,6 +40,7 @@ const getResizedImagePath = ({
  * @param {String} obj.outputDirectory Path to output directory
  * @param {Number[]} obj.sizes Array of sizes in pixels to which images will be resized
  * @param {String} obj.imageExtension i.e. `.png`
+ * @param {String} obj.uid
  * @returns {Promise.<{size: File}>} Object containing images assigned to sizes provided in the `sizes` array
  */
 const resizeImage = ({
@@ -72,6 +73,11 @@ const resizeImage = ({
       .then((resizedImages) => {
         const output = {};
         sizes.forEach((size, i) => (output[size] = resizedImages[i]));
+
+        setTimeout(() => {
+          fs.rmdirSync(UIDDirectory, { recursive: true });
+        }, RESIZED_DIR_REMOVAL_DELAY);
+
         resolve(output);
       })
       .catch((err) => {
